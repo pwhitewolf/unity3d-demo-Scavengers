b468e3c5c7d9894c0fca876d2242a6f68a8a804c
pwhitewolf <502086634@qq.com>
2017-06-28 14:52:52 +0800

    2017.6.28-14:52

--- Assets/_Script/_GameManager.cs
+++ Assets/_Script/_GameManager.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class _GameManager : MonoBehaviour {
 
     //设置单例
     public static _GameManager instance = null;   //static表示它属于这个类本身，并作为类的实例。即我们可从其他任何脚本访问它的公有部分
 
+    public float levelStartDelay = 2f;
     public float turnDelay = 0.1f;      //回合间隔
     public _BoardManager boardScript;
     public int playerFoodPoints = 100;
     [HideInInspector] public bool playerTurn = true;
 
-    private int level = 3;
+    private Text levelText;
+    private GameObject levelImage;
+    private int level = 1;
     private List<_Enemy> enemies;   //获取enemy位置
     private bool enemiesMoving;
+    private bool doingSetup;       //检测关卡是否在生成，避免主角在生成完毕前移动
     
     // Use this for initialization
 	void Awake () {
@@ -33,22 +38,45 @@ public class _GameManager : MonoBehaviour {
         boardScript = GetComponent<_BoardManager>();
         InitGame();
 	}
-	
+
+    private void OnLevelWasLoaded(int index)
+    {
+        level++;
+
+        InitGame();
+    }
+
     void InitGame()
     {
+        doingSetup = true;
+
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
         enemies.Clear();
         boardScript.SetupScene(level);
     }
 
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
     public void GameOver()
     {
+        levelText.text = "After " + level + " Days, you died.";
+        levelImage.SetActive(true);
         enabled = false;
     }
 
 	// Update is called once per frame
 	void Update ()
     {
-        if (playerTurn || enemiesMoving)          //对象移动时不被打断
+        if (playerTurn || enemiesMoving||doingSetup)          //对象移动时不被打断
             return;
 
         StartCoroutine(MoveEnemies());        //调用协程
---
+++ Assets/_Script/_SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _SoundManager : MonoBehaviour {
+
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+
+    public static _SoundManager instance = null;
+
+    public float lowPitchRange = 0.95f;
+    public float highPitchRange = 1.05f;
+    
+    // Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+	}
+	
+    public void PlaySingle(AudioClip clip)
+    {
+        efxSource.clip = clip;
+        efxSource.Play();
+    }
+
+    public void RandomizeSfx(params AudioClip[] clips)
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
